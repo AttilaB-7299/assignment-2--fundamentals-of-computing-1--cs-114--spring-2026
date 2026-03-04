d5f8366fedb3366c30d1ca54bb2c17f8fd202028
AttilaB <attilabloom7299@gmail.com>
2026-03-04 11:03:03 -0500

    finished counting and returning system

--- CountFlips.java
+++ CountFlips.java
@@ -1,4 +1,22 @@
 public class CountFlips {
   public static void main(String[] args) {
+    Coin coin = new Coin();
+
+
+    int heads = 0;
+    int tails = 0;
+
+    for (int i = 0; i < 100; i++){
+      coin.flip();
+      if (coin.isHeads()){
+        System.out.println("Heads");
+        heads++;
+      }else {
+        System.out.println("Tails");
+        tails++;
+      }
+    }
+    System.out.println("Coin landed heads " + heads + " times");
+    System.out.println("the coin landed tails " + tails + " times");
   }
 }
