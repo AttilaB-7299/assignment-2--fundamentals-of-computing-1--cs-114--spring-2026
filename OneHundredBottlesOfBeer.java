7ecdfed04858235252bdcf5a07a1396fe479c1d7
AttilaB <attilabloom7299@gmail.com>
2026-03-05 16:59:38 -0500

    finished 100 bottles of beer

--- OneHundredBottlesOfBeer.java
+++ OneHundredBottlesOfBeer.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
+
 public class OneHundredBottlesOfBeer {
   public static void main(String[] args) {
+    Scanner userInput = new Scanner(System.in);
+    int userInt = userInput.nextInt();
+
+    for (int i = userInt; i > 0; i--) {
+      verse(i);
+    }
+    userInput.close();
+  }
+
+  public static void verse(int count) {
+    System.out.println(count + " bottles of beer on the wall");
+    System.out.println(count + " bottles of beer");
+    System.out.println("If one of those bottles should happen to fall");
+    System.out.println((count - 1) + " bottles of beer on the wall\n");
   }
 }
