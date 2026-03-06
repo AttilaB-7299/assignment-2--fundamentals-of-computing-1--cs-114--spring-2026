9a5b0c9baf5392b1d683e5dceacdd41e96088c22
AttilaB <attilabloom7299@gmail.com>
2026-03-06 07:46:00 -0500

    changed 12 days of christmas to use default instead of fallthrough

--- TheTwelveDaysOfChristmas.java
+++ TheTwelveDaysOfChristmas.java
@@ -21,15 +21,7 @@ public class TheTwelveDaysOfChristmas {
       case 3:
         dayEnding = "rd";
         break;
-      case 4:
-      case 5:
-      case 6:
-      case 7:
-      case 8:
-      case 9:
-      case 10:
-      case 11:
-      case 12:
+      default:
         dayEnding = "th";
         break;
     }
