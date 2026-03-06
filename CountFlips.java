4d69b188ac2d29171ae7ae1a5c48d707d1a0692c
AttilaB <attilabloom7299@gmail.com>
2026-03-06 09:33:00 -0500

    converted all spaces to be tabs

--- CountFlips.java
+++ CountFlips.java
@@ -1,22 +1,22 @@
 public class CountFlips {
-  public static void main(String[] args) {
-    Coin coin = new Coin();
+	public static void main(String[] args) {
+		Coin coin = new Coin();
 
 
-    int heads = 0;
-    int tails = 0;
+		int heads = 0;
+		int tails = 0;
 
-    for (int i = 0; i < 100; i++){
-      coin.flip();
-      if (coin.isHeads()){
-        System.out.println("Heads");
-        heads++;
-      }else {
-        System.out.println("Tails");
-        tails++;
-      }
-    }
-    System.out.println("Coin landed heads " + heads + " times");
-    System.out.println("the coin landed tails " + tails + " times");
-  }
+		for (int i = 0; i < 100; i++){
+			coin.flip();
+			if (coin.isHeads()){
+				System.out.println("Heads");
+				heads++;
+			}else {
+				System.out.println("Tails");
+				tails++;
+			}
+		}
+		System.out.println("Coin landed heads " + heads + " times");
+		System.out.println("the coin landed tails " + tails + " times");
+	}
 }
--- PrintVowelsAndNonVowels.java
+++ PrintVowelsAndNonVowels.java
@@ -1,45 +1,45 @@
 import java.util.Scanner;
 
 public class PrintVowelsAndNonVowels {
-  public static void main(String[] args) {
-    int amountA = 0;
-    int amountE = 0;
-    int amountI = 0;
-    int amountO = 0;
-    int amountU = 0;
-    int nonVowels = 0;
+	public static void main(String[] args) {
+		int amountA = 0;
+		int amountE = 0;
+		int amountI = 0;
+		int amountO = 0;
+		int amountU = 0;
+		int nonVowels = 0;
 
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Please enter a string: ");
-    String input = scanner.nextLine().toLowerCase();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please enter a string: ");
+		String input = scanner.nextLine().toLowerCase();
 
-    for(int i = 0; i < input.length(); i++) {
+		for(int i = 0; i < input.length(); i++) {
 
-      char letter = Character.toLowerCase(input.charAt(i));
+			char letter = Character.toLowerCase(input.charAt(i));
 
-      switch (letter){
-        case 'a':
-          amountA++;
-        case 'e':
-          amountE++;
-        case 'i':
-          amountI++;
-        case 'o':
-          amountO++;
-        case 'u':
-          amountU++;
-        default:
-          nonVowels++;
-      }
-    }
+			switch (letter){
+				case 'a':
+					amountA++;
+				case 'e':
+					amountE++;
+				case 'i':
+					amountI++;
+				case 'o':
+					amountO++;
+				case 'u':
+					amountU++;
+				default:
+					nonVowels++;
+			}
+		}
 
-    System.out.println("That string has " + amountA + " a's");
-    System.out.println("That string has " + amountE + " e's");
-    System.out.println("That string has " + amountI + " i's");
-    System.out.println("That string has " + amountO + " o's");
-    System.out.println("That string has " + amountU + " u's");
-    System.out.println("That string has " + nonVowels + " non-vowels");
+		System.out.println("That string has " + amountA + " a's");
+		System.out.println("That string has " + amountE + " e's");
+		System.out.println("That string has " + amountI + " i's");
+		System.out.println("That string has " + amountO + " o's");
+		System.out.println("That string has " + amountU + " u's");
+		System.out.println("That string has " + nonVowels + " non-vowels");
 
-    scanner.close();
-  }
+		scanner.close();
+	}
 }
